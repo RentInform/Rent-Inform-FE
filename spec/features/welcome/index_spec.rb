7dd1d9f8d137caaeae0dcc4b0e7b7b67d48479ce
Logan Cole <emtlogancole@gmail.com>
2023-06-03 16:45:51 -0500

    test: welcome#index page

---
+++ spec/features/welcome/index_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe "landing page" do
+  before :each do
+    visit root_path
+  end
+
+  it 'has a link to log in' do
+    expect(page).to have_content("Login")
+  end
+
+  it 'has a link to sign up for an account' do
+    expect(page).to have_content("Sign Up")
+  end
+
+  it 'has a description of the app' do
+    expect(page).to have_content("We provide you with code-compliant properties so you can focus on finding the best home for you.")
+  end
+end
