0ae371a6c82dedad46c912b3856b0faee2c3cb53
Stephen McPhee <stephen.t.mcphee@gmail.com>
2023-06-03 18:03:40 -0600

    test: add user registration happy path

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -16,4 +16,11 @@ RSpec.describe "landing page" do
   it 'has a description of the app' do
     expect(page).to have_content("We provide you with code-compliant properties so you can focus on finding the best home for you.")
   end
+
+  it 'sign up link takes you to sign up page' do
+    click_link "Sign Up"
+
+    expect(current_path).to eq(sign_up_path)
+    expect(page).to have_content('RentInform#Sign Up')
+  end
 end
