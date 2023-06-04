8f717143db0650ac463e3ef8c580e540d966d583
Logan Cole <emtlogancole@gmail.com>
2023-06-04 12:48:27 -0500

    feat: show page has button to home page and find property section" ;

--- spec/features/dashboard/show_spec.rb
+++ spec/features/dashboard/show_spec.rb
@@ -20,4 +20,38 @@ RSpec.describe "dashboard show page" do
 
     expect(current_path).to eq(root_path)
   end
+
+
+  it 'has a button to the home page'do
+    user = create(:user)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit dashboard_path
+
+    expect(page).to have_button("Home")
+
+    click_button "Home"
+
+    expect(current_path).to eq(root_path)
+  end
+
+  it 'has a find property subheader' do
+    user = create(:user)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit dashboard_path
+    within"#find-property" do
+      expect(page).to have_content("Find a Property")
+    end
+  end
+
+  xit 'has a search bar' do
+    user = create(:user)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit dashboard_path
+
+    expect(page).to have_field(:search_street)
+    expect(page).to have_field(:search_zip)
+  end
 end
