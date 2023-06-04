334c38a0c84c09fccc153f482a1b7638067992e6
Logan Cole <emtlogancole@gmail.com>
2023-06-04 12:06:54 -0500

    chore: mock service
    Co-authored-by: Stephen McPhee <stephen.t.mcphee@gmail.com>

--- spec/services/rent_inform_service_spec.rb
+++ spec/services/rent_inform_service_spec.rb
@@ -2,15 +2,21 @@ require 'rails_helper'
 
 RSpec.describe RentInformService do
   before(:each) do
-    WebMock.stub_request(:get, "https://test.com/return_json/properties.json/api/v0/search?street=123+Main+Street&zip=19148")
-     .to_return(status: 200, body: File.read('./spec/fixtures/property_1.json'))
+    stub_request(:get, "https://test.com/api/v0/search?street=123%20Main%20Street&zip=19148").
+         with(
+           headers: {
+          'Accept'=>'*/*',
+          'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+          'User-Agent'=>'Faraday v2.7.5'
+           })
+    .to_return(status: 200, body: File.read('./spec/fixtures/property_1.json'))
   end
 
   describe 'instance methods' do
     it '#get_property' do
       service = RentInformService.new
       property = service.get_property('https://test.com/return_json/properties.json', '123 Main Street', '19148')
-
+      
       expect(property).to be_a(Hash)
 
       expect(property).to have_key(:data)
@@ -22,7 +28,7 @@ RSpec.describe RentInformService do
       expect(property[:data][:type]).to be_a(String)
 
       attributes = property[:data][:attributes]
-      expect(attributes[:address]).to eq('123 Main Street')
+      expect(attributes[:street]).to eq('123 Main Street')
       expect(attributes[:city]).to eq('Philadelphia')
       expect(attributes[:state]).to eq('PA')
       expect(attributes[:zip]).to eq('19148')
