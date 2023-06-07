bcd639679e03bb5b81049802fb038ad395e19439
Logan Cole <emtlogancole@gmail.com>
2023-06-07 18:25:18 -0500

    test: dashboard search finds no property
    Co-authored-by: Stephen McPhee <stephen.t.mcphee@gmail.com>
    Co-authored-by: Caroline Peri <77258069+cariperi@users.noreply.github.com>

--- spec/features/dashboard/show_spec.rb
+++ spec/features/dashboard/show_spec.rb
@@ -193,29 +193,39 @@ RSpec.describe "dashboard show page" do
       expect(page).to have_content("Save a property to view it here!")
     end
 
-    xit 'does not render property if no result is found' do
-      #this test is all wrong, needs it's own stub probably
-      #and to call .to_not have_content
-     
-      # @user = create(:user)
-      # allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+    it 'does not render property if no result is found' do
+      stub_request(:get, "https://sheltered-harbor-92742.herokuapp.com/api/v0/search?street=65891%20Not%20a%20real%20street&zip=84606")
+      .with(
+        headers: {
+       'Accept'=>'*/*',
+       'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+       'User-Agent'=>'Faraday v2.7.5'
+        })
+      .to_return(status: 200, body: File.read('./spec/fixtures/no_property.json'))
 
-      # visit dashboard_path
+      stub_request(:get, "https://sheltered-harbor-92742.herokuapp.com/api/v0/user_properties?user_id=338")
+        .with(
+           headers: {
+          'Accept'=>'*/*',
+          'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+          'User-Agent'=>'Faraday v2.7.5'
+           })
+        .to_return(status: 200, body: File.read('./spec/fixtures/no_user_properties.json'))
 
-      # fill_in :search_street, with: "123 Main Street"
-      # fill_in :search_zip, with: "19148"
-      # click_button "Search"
-      # expect(current_path).to eq(dashboard_path)
-      
-      # within "#Result" do
-      #   expect(page).to have_content("This Property is on the Certified Rentals list!")
-      #   expect(page).to have_content("123 Main Street")
-      #   expect(page).to have_content("19148")
-      #   expect(page).to have_content("Walk Score: 89")
-      #   expect(page).to have_content("Bike Score: 23")
-      #   expect(page).to have_content("Transit Score: 57")
-      #   expect(page).to have_content("Safety Score: 99")
-      # end
+      user = create(:user, id: 338)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      visit dashboard_path
+
+      fill_in :search_street, with: "65891 Not a real street"
+      fill_in :search_zip, with: "84606"
+      click_button "Search"
+      expect(current_path).to eq(dashboard_path)
+      save_and_open_page
+      within "#Result" do
+        expect(page).to_not have_content("This Property is on the Certified Rentals list!")
+        expect(page).to have_content("This property is not on the Certified Rentals list.")
+      end
     end
   end
 end
