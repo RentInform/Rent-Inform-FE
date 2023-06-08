3a81351a04796ed3bd389f83177670fc3fa7289c
Caroline Peri <caroline.anne.peri@gmail.com>
2023-06-08 00:23:31 -0400

    test: add tests for saving and unsaving a property functionality

--- spec/features/dashboard/show_spec.rb
+++ spec/features/dashboard/show_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe "dashboard show page" do
+RSpec.describe 'dashboard show page' do
   describe 'happy path' do
     before :each do
       @user = create(:user, id: 420)
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
-   
-      stub_request(:get, "https://sheltered-harbor-92742.herokuapp.com/api/v0/user_properties?property_id=2&user_id=420")
+
+      stub_request(:get, 'https://sheltered-harbor-92742.herokuapp.com/api/v0/user_property?property_id=2&user_id=420')
       .with(
         headers: {
        'Accept'=>'*/*',
@@ -15,7 +15,7 @@ RSpec.describe "dashboard show page" do
         })
       .to_return(status: 200, body: File.read('./spec/fixtures/property_2.json'))
 
-      stub_request(:get, "https://sheltered-harbor-92742.herokuapp.com/api/v0/search?street=123%20Main%20Street&zip=19148")
+      stub_request(:get, 'https://sheltered-harbor-92742.herokuapp.com/api/v0/search?street=123%20Main%20Street&zip=19148')
       .with(
         headers: {
       'Accept'=>'*/*',
@@ -23,8 +23,8 @@ RSpec.describe "dashboard show page" do
       'User-Agent'=>'Faraday v2.7.5'
         })
       .to_return(status: 200, body: File.read('./spec/fixtures/property_1.json'))
-      
-      stub_request(:get, "https://sheltered-harbor-92742.herokuapp.com/api/v0/user_properties?user_id=420")
+
+      stub_request(:get, 'https://sheltered-harbor-92742.herokuapp.com/api/v0/user_properties?user_id=420')
       .with(
         headers: {
       'Accept'=>'*/*',
@@ -33,37 +33,34 @@ RSpec.describe "dashboard show page" do
         })
       .to_return(status: 200, body: File.read('./spec/fixtures/user_properties_1.json'))
     end
+
     it 'has a button to log out' do
-      
-
       visit dashboard_path
-
-      expect(page).to have_button("Log Out")
+      expect(page).to have_button('Log Out')
     end
 
     it 'logs out the user' do
       visit dashboard_path
 
-      click_button "Log Out"
+      click_button 'Log Out'
 
       expect(current_path).to eq(root_path)
     end
 
-
-    it 'has a button to the home page'do
+    it 'has a button to the home page' do
       visit dashboard_path
 
-      expect(page).to have_button("Home")
+      expect(page).to have_button('Home')
 
-      click_button "Home"
+      click_button 'Home'
 
       expect(current_path).to eq(root_path)
     end
 
     it 'has a find property subheader' do
       visit dashboard_path
-      within"#find-property" do
-        expect(page).to have_content("Find a Property")
+      within'#find-property' do
+        expect(page).to have_content('Find a Property')
       end
     end
 
@@ -73,93 +70,165 @@ RSpec.describe "dashboard show page" do
       expect(page).to have_field(:search_street)
       expect(page).to have_field(:search_zip)
     end
-    
+
     it 'searches' do
       visit dashboard_path
 
-      fill_in :search_street, with: "123 Main Street"
-      fill_in :search_zip, with: "19148"
-      click_button "Search"
+      fill_in :search_street, with: '123 Main Street'
+      fill_in :search_zip, with: '19148'
+      click_button 'Search'
       expect(current_path).to eq(dashboard_path)
-      
-      within "#Result" do
-        expect(page).to have_content("This Property is on the Certified Rentals list!")
-        expect(page).to have_content("123 Main Street")
-        expect(page).to have_content("19148")
-        expect(page).to have_content("Walk Score: 89")
-        expect(page).to have_content("Bike Score: 23")
-        expect(page).to have_content("Transit Score: 57")
-        expect(page).to have_content("Safety Score: 99")
+
+      within '#Result' do
+        expect(page).to have_content('Certified to Rent')
+        expect(page).to have_content('123 Main Street')
+        expect(page).to have_content('19148')
+        expect(page).to have_content('Walk Score: 89')
+        expect(page).to have_content('Bike Score: 23')
+        expect(page).to have_content('Transit Score: 57')
+        expect(page).to have_content('Safety Score: 99')
       end
     end
 
     it 'gets a list of user_properties' do
       visit dashboard_path
 
-      within "#Saved" do
-        expect(page).to have_content("100 Nowhere Street")
-        expect(page).to have_content("456 Center Street")
-        expect(page).to have_content("789 Memory Lane")
+      within '#Saved' do
+        expect(page).to have_content('100 Nowhere Street')
+        expect(page).to have_content('456 Center Street')
+        expect(page).to have_content('789 Memory Lane')
       end
     end
 
     it 'displays property details when user clicks more details' do
       visit dashboard_path
 
-      within "#property-2" do
-        click_button "More Details"
+      within '#property-2' do
+        click_button 'More Details'
         expect(current_path).to eq('/dashboard')
       end
 
-      within "#Result" do
-        expect(page).to have_content("This Property is on the Certified Rentals list!")
-        expect(page).to have_content("456 Center Street")
-        expect(page).to have_content("19148")
-        expect(page).to have_content("Walk Score: 55")
-        expect(page).to have_content("Bike Score: 73")
-        expect(page).to have_content("Transit Score: 97")
-        expect(page).to have_content("Safety Score: 65")
+      within '#Result' do
+        expect(page).to have_content('Certified to Rent')
+        expect(page).to have_content('456 Center Street')
+        expect(page).to have_content('19148')
+        expect(page).to have_content('Walk Score: 55')
+        expect(page).to have_content('Bike Score: 73')
+        expect(page).to have_content('Transit Score: 97')
+        expect(page).to have_content('Safety Score: 65')
       end
     end
 
     it 'can show a searched property, then show more details' do
       visit dashboard_path
 
-      fill_in :search_street, with: "123 Main Street"
-      fill_in :search_zip, with: "19148"
-      click_button "Search"
+      fill_in :search_street, with: '123 Main Street'
+      fill_in :search_zip, with: '19148'
+      click_button 'Search'
       expect(current_path).to eq(dashboard_path)
 
-      within "#Result" do
-        expect(page).to have_content("This Property is on the Certified Rentals list!")
-        expect(page).to have_content("123 Main Street")
-        expect(page).to have_content("19148")
-        expect(page).to have_content("Walk Score: 89")
-        expect(page).to have_content("Bike Score: 23")
-        expect(page).to have_content("Transit Score: 57")
-        expect(page).to have_content("Safety Score: 99")
+      within '#Result' do
+        expect(page).to have_content('Certified to Rent')
+        expect(page).to have_content('123 Main Street')
+        expect(page).to have_content('19148')
+        expect(page).to have_content('Walk Score: 89')
+        expect(page).to have_content('Bike Score: 23')
+        expect(page).to have_content('Transit Score: 57')
+        expect(page).to have_content('Safety Score: 99')
       end
 
-      within "#property-2" do
-        click_button "More Details"
+      within '#property-2' do
+        click_button 'More Details'
         expect(current_path).to eq('/dashboard')
       end
 
-      within "#Result" do
-        expect(page).to have_content("This Property is on the Certified Rentals list!")
-        expect(page).to have_content("456 Center Street")
-        expect(page).to have_content("19148")
-        expect(page).to have_content("Walk Score: 55")
-        expect(page).to have_content("Bike Score: 73")
-        expect(page).to have_content("Transit Score: 97")
-        expect(page).to have_content("Safety Score: 65")
+      within '#Result' do
+        expect(page).to have_content('Certified to Rent')
+        expect(page).to have_content('456 Center Street')
+        expect(page).to have_content('19148')
+        expect(page).to have_content('Walk Score: 55')
+        expect(page).to have_content('Bike Score: 73')
+        expect(page).to have_content('Transit Score: 97')
+        expect(page).to have_content('Safety Score: 65')
+      end
+    end
+
+    it 'a user can save a property' do
+      stub_request(:post, 'https://sheltered-harbor-92742.herokuapp.com/api/v0/user_property?property_id=1&user_id=420').
+      to_return(status: 200, body: File.read('./spec/fixtures/save_property.json'))
+
+      visit dashboard_path
+
+      fill_in :search_street, with: '123 Main Street'
+      fill_in :search_zip, with: '19148'
+      click_button 'Search'
+
+      within '#Result' do
+        click_button 'Save'
+      end
+
+      expect(current_path).to eq(dashboard_path)
+
+      stub_request(:get, 'https://sheltered-harbor-92742.herokuapp.com/api/v0/user_properties?user_id=420')
+      .with(
+        headers: {
+      'Accept'=>'*/*',
+      'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+      'User-Agent'=>'Faraday v2.7.5'
+        })
+      .to_return(status: 200, body: File.read('./spec/fixtures/user_properties_2.json'))
+
+      visit dashboard_path
+
+      within '#Saved' do
+        expect(page).to have_content('123 Main Street')
+
+        within '#property-1' do
+          expect(page).to_not have_button 'Save'
+          expect(page).to have_button 'Unsave'
+        end
+      end
+    end
+
+    it 'a user can delete a property' do
+      stub_request(:delete, 'https://sheltered-harbor-92742.herokuapp.com/api/v0/user_property?property_id=1&user_id=420').
+      to_return(status: 200, body: File.read('./spec/fixtures/unsave_property.json'))
+
+      stub_request(:get, 'https://sheltered-harbor-92742.herokuapp.com/api/v0/user_properties?user_id=420')
+      .with(
+        headers: {
+      'Accept'=>'*/*',
+      'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+      'User-Agent'=>'Faraday v2.7.5'
+        })
+      .to_return(status: 200, body: File.read('./spec/fixtures/user_properties_2.json'))
+
+      visit dashboard_path
+
+      within '#property-1' do
+        click_button 'Unsave'
+      end
+
+      stub_request(:get, 'https://sheltered-harbor-92742.herokuapp.com/api/v0/user_properties?user_id=420')
+      .with(
+        headers: {
+      'Accept'=>'*/*',
+      'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+      'User-Agent'=>'Faraday v2.7.5'
+        })
+      .to_return(status: 200, body: File.read('./spec/fixtures/user_properties_1.json'))
+
+      visit dashboard_path
+
+      within '#Saved' do
+        expect(page).to_not have_content('123 Main Street')
       end
     end
   end
 
   describe 'sad path' do
     before :each do
-      stub_request(:get, "https://sheltered-harbor-92742.herokuapp.com/api/v0/search?street=123%20Main%20Street&zip=19148")
+      stub_request(:get, 'https://sheltered-harbor-92742.herokuapp.com/api/v0/search?street=123%20Main%20Street&zip=19148')
       .with(
         headers: {
       'Accept'=>'*/*',
@@ -168,16 +237,18 @@ RSpec.describe "dashboard show page" do
         })
       .to_return(status: 200, body: File.read('./spec/fixtures/property_1.json'))
     end
+
     it 'redirects to home page if not logged in' do
       visit dashboard_path
 
-      expect(page).to have_content("You must be logged in to view this page")
+      expect(page).to have_content('You must be logged in to view this page')
       expect(current_path).to eq(root_path)
     end
 
     it 'does not render user_properties if none are found' do
       user = create(:user)
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
       stub_request(:get, "https://sheltered-harbor-92742.herokuapp.com/api/v0/user_properties?user_id=#{user.id}")
       .with(
         headers: {
@@ -190,11 +261,11 @@ RSpec.describe "dashboard show page" do
 
       visit dashboard_path
 
-      expect(page).to have_content("Save a property to view it here!")
+      expect(page).to have_content('Save a property to view it here!')
     end
 
     it 'does not render property if no result is found' do
-      stub_request(:get, "https://sheltered-harbor-92742.herokuapp.com/api/v0/search?street=65891%20Not%20a%20real%20street&zip=84606")
+      stub_request(:get, 'https://sheltered-harbor-92742.herokuapp.com/api/v0/search?street=65891%20Not%20a%20real%20street&zip=84606')
       .with(
         headers: {
        'Accept'=>'*/*',
@@ -203,7 +274,7 @@ RSpec.describe "dashboard show page" do
         })
       .to_return(status: 200, body: File.read('./spec/fixtures/no_property.json'))
 
-      stub_request(:get, "https://sheltered-harbor-92742.herokuapp.com/api/v0/user_properties?user_id=338")
+      stub_request(:get, 'https://sheltered-harbor-92742.herokuapp.com/api/v0/user_properties?user_id=338')
         .with(
            headers: {
           'Accept'=>'*/*',
@@ -217,14 +288,14 @@ RSpec.describe "dashboard show page" do
 
       visit dashboard_path
 
-      fill_in :search_street, with: "65891 Not a real street"
-      fill_in :search_zip, with: "84606"
-      click_button "Search"
+      fill_in :search_street, with: '65891 Not a real street'
+      fill_in :search_zip, with: '84606'
+      click_button 'Search'
       expect(current_path).to eq(dashboard_path)
 
-      within "#Result" do
-        expect(page).to_not have_content("This Property is on the Certified Rentals list!")
-        expect(page).to have_content("This property is not on the Certified Rentals list.")
+      within '#Result' do
+        expect(page).to_not have_content('Certified to Rent')
+        expect(page).to have_content('This property is not on the Certified Rentals list.')
       end
     end
   end
