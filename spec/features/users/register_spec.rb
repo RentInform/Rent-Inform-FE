703222b39e0ee8efea9df2b3dc7d3345753abc8f
Grace Joh <graceehjoh@gmail.com>
2023-06-08 00:15:12 -0500

    style/test: add tailwindcss to sign up page and update test

--- spec/features/users/register_spec.rb
+++ spec/features/users/register_spec.rb
@@ -5,38 +5,32 @@ RSpec.describe "user registration" do
     visit sign_up_path
   end
 
-  it 'as a user, I see a disclaimer about google' do
-    within '#disclaimer' do
-      expect(page).to have_content('RentInform uses Google for new user creation')
-    end
-  end
-
-  it 'as a user I see decorative text' do
-    within '#decorative' do
+  it 'as a user I see the title text' do
+    within '#title' do
       expect(page).to have_content('Find your home')
     end
   end
 
   it 'as a user I see a button to sign up with google' do
-    within '#google' do
-      expect(page).to have_link('Sign Up with Google')
+    within '#google-link' do
+      expect(page).to have_css('a[href="https://accounts.google.com/signup/v2/createaccount?biz=false"]')
     end
   end
 
   it 'as a user I see a button to log in' do
     within '#login' do
       expect(page).to have_content('Already have an account?')
-      expect(page).to have_link('Log In')
+      expect(page).to have_link('Log in')
     end
   end
 
   it 'as a user when I click the login button, I am take to the log in page' do
     within '#login' do
-      click_link 'Log In'
+      click_link 'Log in'
       expect(current_path).to eq(login_path)
     end
 
     visit login_path
-    expect(page).to have_content('RentInform#Log In')
+    expect(page).to have_content('RentInform')
   end
 end
