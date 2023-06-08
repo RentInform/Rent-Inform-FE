98b4c54f70f48e7216d8538cae2b657998b79aa1
Caroline Peri <caroline.anne.peri@gmail.com>
2023-06-08 00:24:46 -0400

    chore: set up post_url and delete_url methods

--- app/services/rent_inform_service.rb
+++ app/services/rent_inform_service.rb
@@ -1,5 +1,4 @@
 class RentInformService
-
   def get_property(street, zip)
     get_url("/api/v0/search?street=#{street}&zip=#{zip}")
   end
@@ -7,20 +6,37 @@ class RentInformService
   def get_user_properties(user_id)
     get_url("/api/v0/user_properties?user_id=#{user_id}")
   end
-  
+
   def get_property_by_id(user_id, property_id)
-    get_url("/api/v0/user_properties?user_id=#{user_id}&property_id=#{property_id}")
+    get_url("/api/v0/user_property?user_id=#{user_id}&property_id=#{property_id}")
+  end
+
+  def save_property(user_id, property_id)
+    post_url("/api/v0/user_property?user_id=#{user_id}&property_id=#{property_id}")
+  end
+
+  def unsave_property(user_id, property_id)
+    delete_url("/api/v0/user_property?user_id=#{user_id}&property_id=#{property_id}")
   end
 
   private
 
-    def get_url(url)
-      response = conn.get(url)
-      JSON.parse(response.body, symbolize_names: true)
-    end
+  def get_url(url)
+    response = conn.get(url)
+    JSON.parse(response.body, symbolize_names: true)
+  end
 
-    def conn
-      Faraday.new(url: 'https://sheltered-harbor-92742.herokuapp.com') do |faraday|
-      end
+  def post_url(url)
+    response = conn.post(url)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def delete_url(url)
+    conn.delete(url)
+  end
+
+  def conn
+    Faraday.new(url: 'https://sheltered-harbor-92742.herokuapp.com') do |faraday|
     end
+  end
 end
