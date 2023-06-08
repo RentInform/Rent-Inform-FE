51cd925ea5982b1bb33bc4c3a8fa8509386f92f3
Caroline Peri <caroline.anne.peri@gmail.com>
2023-06-08 00:24:10 -0400

    feat: update controllers and add actions for create and delete user property endpoints

---
+++ app/controllers/user_properties_controller.rb
@@ -0,0 +1,11 @@
+class UserPropertiesController < ApplicationController
+  def create
+    RentInformService.new.save_property(params[:user_id], params[:property_id])
+    redirect_to dashboard_path
+  end
+
+  def destroy
+    RentInformService.new.unsave_property(params[:user_id], params[:property_id])
+    redirect_to dashboard_path
+  end
+end
