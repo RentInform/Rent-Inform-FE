d6f911e9dc182ff478f083fb21402c650718d913
Logan Cole <emtlogancole@gmail.com>
2023-06-22 15:51:58 -0500

    feat: philly map in property#show wip
    Co-authored-by: Caroline Peri <77258069+cariperi@users.noreply.github.com>

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def prop_ids(user_properties)
     user_properties.map(&:id)
   end
+
+  def map(lat, lon)
+    "https://www.mapquestapi.com/staticmap/v5/map?locations=#{lat}, #{lon}&size=400, 400&zoom=12&center=#{lat}, #{lon}&key=#{ENV['MAPQUEST_KEY']}"
+  end
 end
