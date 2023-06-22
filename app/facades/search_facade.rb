cf44757beb61a7df8460bf0512647c23cfaeffd0
Caroline Peri <caroline.anne.peri@gmail.com>
2023-06-22 12:42:21 -0400

    chore/refactor: update format property method to remove scores

--- app/facades/search_facade.rb
+++ app/facades/search_facade.rb
@@ -6,8 +6,8 @@ class SearchFacade
   end
 
   def property
-    return SearchError.new if @street.count("a-zA-Z") == 0  
-    
+    return SearchError.new if @street.count("a-zA-Z").zero?
+
     property ||= service.get_property(@street, @zip)
     if property[:data].nil?
       SearchError.new
@@ -22,10 +22,6 @@ class SearchFacade
     {
       street: property[:data][:attributes][:street],
       zip: property[:data][:attributes][:zip],
-      bike_score: property[:data][:attributes][:bike_score],
-      walk_score: property[:data][:attributes][:walk_score],
-      safety_score: property[:data][:attributes][:safety_score],
-      transit_score: property[:data][:attributes][:transit_score],
       city: property[:data][:attributes][:city],
       state: property[:data][:attributes][:state],
       id: property[:data][:id]
