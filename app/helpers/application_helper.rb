245d77c526026b0024a79fe32ff8f7e6efdc769d
Caroline Peri <caroline.anne.peri@gmail.com>
2023-06-27 17:21:49 -0400

    feat: update Property Show view to display parks and handle error (no parks found)
    Co-authored-by: Stephen McPhee <120869196+SMcPhee19@users.noreply.github.com>

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,6 +4,14 @@ module ApplicationHelper
   end
 
   def map(lat, lon)
-    "https://www.mapquestapi.com/staticmap/v5/map?locations=#{lat}, #{lon}&size=400, 400&zoom=12&center=#{lat}, #{lon}&key=#{ENV['MAPQUEST_KEY']}"
+    "https://www.mapquestapi.com/staticmap/v5/map?locations=#{lat}, #{lon}&size=400, 400&zoom=12&center=#{lat}, #{lon}&defaultMarker=marker-lg-1D4ED8&key=#{ENV['MAPQUEST_KEY']}"
+  end
+
+  def format_parks(parks)
+    return [{ name: 'No parks found.', street: '' }] if parks.empty?
+
+    [{ name: parks[:park_1_name], street: parks[:park_1_street] },
+     { name: parks[:park_2_name], street: parks[:park_2_street] },
+     { name: parks[:park_3_name], street: parks[:park_3_street] }]
   end
 end
